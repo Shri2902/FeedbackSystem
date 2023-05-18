7198601444a160d61a668ae35c52bdc85bd232bf
Nishal <itachiuchiha290502@gmail.com>
2023-05-18 18:25:50 +0530

    4th

--- nav1.php
+++ nav1.php
@@ -1,26 +1,23 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="/project">iSecure</a>
+  <a class="navbar-brand" href="/project">Project</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
+    <!-- <ul class="navbar-nav mr-auto">
 
       <li class="nav-item">
-        <a class="nav-link" href="/project/feedback.php">Feedback</a>
+        <a class="nav-link" href="/project/feedback.php">Login</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="/project/index.php">Home</a>
+        <a class="nav-link" href="/project/index.php">Sign Up</a>
       </li>
-
-
-
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
+    </ul> -->
+    <!-- <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+    </form> -->
   </div>
 </nav>
